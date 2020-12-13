c7259e2f12fc737e8c1cde89dec5888ebb073e39
turpp <white.tripp@icloud.com>
2020-12-12 19:40:39 -0600

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,16 +1,27 @@
 class AttractionsController < ApplicationController
 
     def new
-
+        @attraction=Attraction.new
 
     end 
 
     def create
+        # byebug
+        @attraction=Attraction.create(attraction_params)
+        redirect_to attraction_path(@attraction)
 
     end
 
     def edit
+        @attraction=Attraction.find_by(id: params[:id])
 
+    end
+
+    def update
+        # byebug
+        a=Attraction.find_by(id: params[:id])
+        a.update(attraction_params)
+        redirect_to attraction_path(a)
     end
     def index 
         @attractions=Attraction.all
@@ -37,5 +48,10 @@ class AttractionsController < ApplicationController
         #tickets
         #mood
         redirect_to user_path(session[:user_id]), notice: ride.take_ride
+    end
+
+    private
+    def attraction_params
+        params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
     end
 end
